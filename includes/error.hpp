108381de73996453d0e4fc2c833a4b020e811872
fZpHr <afterrush@live.fr>
2024-06-06 15:15:21 +0200

    add RPL

--- includes/error.hpp
+++ includes/error.hpp
@@ -6,24 +6,13 @@
 /*   By: hbelle <hbelle@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 16:19:54 by hbelle            #+#    #+#             */
-/*   Updated: 2024/06/05 17:20:49 by hbelle           ###   ########.fr       */
+/*   Updated: 2024/06/06 15:11:15 by hbelle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef ERROR_HPP
 # define ERROR_HPP
-
-
-# define RPL_LISTSTART(input)					"321 " + input + " Channel :Users  Name"
-# define RPL_LIST(input, chan, users, topic)	"322 " + input + " " + chan + " " + users + " :" + topic
-# define RPL_LISTEND(input)						"323 " + input + " :End of /LIST"
-# define RPL_CHANNELMODEIS(input, chan, md)		"324 " + input + " " + chan + " " + md
-# define RPL_NOTOPIC(input, chan)				"331 " + input + " " + chan + " :No topic is set"
-# define RPL_TOPIC(input, chan, topic)			"332 " + input + " " + chan + " :" + topic
-# define RPL_INVITING(input, nick, chan)		"341 " + input + " " + nick + " " + chan
-# define RPL_NAMREPLY(input, chan, nicks)		"353 " + input + " = " + chan + " :" + nicks
-# define RPL_ENDOFNAMES(input, chan)			"366 " + input + " " + chan + " :End of /NAMES list"
 
 # define ERR_NOSUCHNICK(input, nick)			"401 " + input + " " + nick + " :No such nick/channel"
 # define ERR_NOSUCHCHANNEL(input, chan)			"403 " + input + " " + chan + " :No such channel"
--- includes/response.hpp
+++ includes/response.hpp
@@ -3,16 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   response.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cpeterea <cpeterea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hbelle <hbelle@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 14:58:56 by cpeterea          #+#    #+#             */
-/*   Updated: 2024/06/06 15:01:09 by cpeterea         ###   ########.fr       */
+/*   Updated: 2024/06/06 15:11:57 by hbelle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RESPONSE_HPP
 # define RESPONSE_HPP
 
-# define RPL_WELCOME(nick, user, host)					"001 " + "Welcome to the Internet Relay Network" + "<" + nick ">!<"user">@<"host">"
-
+# define RPL_WELCOME(n)							"001 " + n + " :Welcome to the ft_irc Network " + n + "!"
+# define RPL_YOURHOST(n, serv, ver)				"002 " + n + " :Your host is " + serv + ", running version " + ver
+# define RPL_CREATED(n, date)					"003 " + n + " :This server was created " + date
+# define RPL_LISTSTART(input)					"321 " + input + " Channel :Users  Name"
+# define RPL_LIST(input, chan, users, topic)	"322 " + input + " " + chan + " " + users + " :" + topic
+# define RPL_LISTEND(input)						"323 " + input + " :End of /LIST"
+# define RPL_CHANNELMODEIS(input, chan, md)		"324 " + input + " " + chan + " " + md
+# define RPL_NOTOPIC(input, chan)				"331 " + input + " " + chan + " :No topic is set"
+# define RPL_TOPIC(input, chan, topic)			"332 " + input + " " + chan + " :" + topic
+# define RPL_INVITING(input, nick, chan)		"341 " + input + " " + nick + " " + chan
+# define RPL_NAMREPLY(input, chan, nicks)		"353 " + input + " = " + chan + " :" + nicks
+# define RPL_ENDOFNAMES(input, chan)			"366 " + input + " " + chan + " :End of /NAMES list"
 #endif
